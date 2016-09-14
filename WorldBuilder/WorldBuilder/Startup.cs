e433e188eacf62e8086e1d35f63ad2373e48fad1
Eric <ericdbye@gmail.com>
2016-09-13 21:43:52 -0500

    Initial check in

---
+++ WorldBuilder/WorldBuilder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorldBuilder.Startup))]
+namespace WorldBuilder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
